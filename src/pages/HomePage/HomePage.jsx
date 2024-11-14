c54a50dc95819d4702d3d2cec2f182352a89b576
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-14 15:22:26 +0600

    [feat](HomePage): add Helmet for SEO optimization with dynamic title

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "../../components/Home/Banner/Banner";
 import Newsletter from "../../components/Home/Newsletter/Newsletter";
 
 const HomePage = () => {
   return (
     <>
+      <Helmet>
+        <title>Trippie - Home</title>
+      </Helmet>
       <Banner />
       <Newsletter />
     </>
