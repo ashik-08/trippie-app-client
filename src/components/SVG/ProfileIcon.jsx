248889db85ba06c7cef6de45b5bf0652fa089434
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-17 13:41:46 +0600

    [feat](svg): add dashboard navbar icons svg

---
+++ src/components/SVG/ProfileIcon.jsx
@@ -0,0 +1,60 @@
+const ProfileIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="16"
+      height="16"
+      x="0"
+      y="0"
+      viewBox="0 0 32 32"
+      xmlSpace="preserve"
+      className=""
+    >
+      <g>
+        <g data-name="Layer 2">
+          <path
+            d="M16 17a6 6 0 1 1 6-6 6 6 0 0 1-6 6zm0-10a4 4 0 1 0 4 4 4 4 0 0 0-4-4z"
+            fill="#000000"
+            opacity="1"
+            data-original="#000000"
+            className=""
+          ></path>
+          <path
+            d="M16 31a15 15 0 0 1-11.59-5.49l-.52-.64.52-.63a15 15 0 0 1 23.18 0l.52.63-.52.64A15 15 0 0 1 16 31zm-9.49-6.12a13 13 0 0 0 19 0 13 13 0 0 0-19 0z"
+            fill="#000000"
+            opacity="1"
+            data-original="#000000"
+            className=""
+          ></path>
+          <path
+            d="M16 31a15 15 0 1 1 11.59-5.49A15 15 0 0 1 16 31zm0-28a13 13 0 1 0 13 13A13 13 0 0 0 16 3z"
+            fill="#000000"
+            opacity="1"
+            data-original="#000000"
+            className=""
+          ></path>
+          <path
+            d="M5.18 24.88S15.25 36.13 25.5 26l1.32-1.12S18.26 16 9.57 21.33z"
+            fill="#000000"
+            opacity="1"
+            data-original="#000000"
+            className=""
+          ></path>
+          <circle
+            cx="16"
+            cy="11"
+            r="5"
+            fill="#000000"
+            opacity="1"
+            data-original="#000000"
+            className=""
+          ></circle>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default ProfileIcon;
