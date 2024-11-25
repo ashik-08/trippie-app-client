abe952c4279106edd8bfb69434de6edd22fe5802
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-25 20:40:17 +0600

    [feat](video): update video and video screen size

--- src/components/Home/VideoPlayer/VideoPlayer.jsx
+++ src/components/Home/VideoPlayer/VideoPlayer.jsx
@@ -1,17 +1,25 @@
 const VideoPlayer = () => {
   return (
-    <div className="relative">
-      {/* Video Element */}
-      <video autoPlay loop muted playsInline>
-        <source
-          src="https://res.cloudinary.com/ashikur-rahman/video/upload/v1732535435/foggyMorningNarrowRoad_rzslnt.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+    <div className="lg:h-[85vh]">
+      <div className="relative w-full h-full">
+        {/* Video Element */}
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-full lg:object-cover"
+        >
+          <source
+            src="https://res.cloudinary.com/ashikur-rahman/video/upload/v1732545365/cloudyMountain_vzwtb3.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
 
-      {/* Dark Overlay */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black/30"></div>
+        {/* Dark Overlay */}
+        <div className="absolute top-0 left-0 w-full h-full bg-black/30"></div>
+      </div>
     </div>
   );
 };
