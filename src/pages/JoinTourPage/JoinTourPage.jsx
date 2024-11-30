e29ade511bce79dce8233f2986985526db0bcf69
ashik-08 <ashikurrahman.8october@gmail.com>
2024-11-30 17:53:44 +0600

    [feat](joinTour): add join tour page with joinTourBoard

---
+++ src/pages/JoinTourPage/JoinTourPage.jsx
@@ -0,0 +1,21 @@
+import { Helmet } from "react-helmet-async";
+import JoinTourBoard from "../../components/JoinTour/JoinTourBoard";
+import Banner from "../../components/Shared/Banner/Banner";
+
+const JoinTourPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Trippie - Join Tour</title>
+      </Helmet>
+      <Banner
+        hero="bg-joinTour"
+        title="Find the best relaxing Tour for you"
+        boldTitle="With Us!"
+      />
+      <JoinTourBoard />
+    </>
+  );
+};
+
+export default JoinTourPage;
