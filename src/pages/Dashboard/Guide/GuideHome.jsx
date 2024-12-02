4478b4ccc303e638f182cf67288a2980903cdf97
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-02 08:10:32 +0600

    [feat](home): add guide home with subscription card

--- src/pages/Dashboard/Guide/GuideHome.jsx
+++ src/pages/Dashboard/Guide/GuideHome.jsx
@@ -1,27 +1,3 @@
-// import { Helmet } from "react-helmet-async";
-// import SubscriptionCard from "./SubscriptionCard";
-
-// const GuideHome = () => {
-//   return (
-//     <>
-//       <Helmet>
-//         <title>Trippie - Tour Guide</title>
-//       </Helmet>
-//       <h1 className="text-2xl lg:text-3xl font-medium mb-8">Dashboard</h1>
-//       <SubscriptionCard
-//         planName="Standard Plan"
-//         price="200"
-//         validityStart="2024-12-01"
-//         validityEnd="2024-12-31"
-//         status="active"
-//         renewalReminder={true}
-//       />
-//     </>
-//   );
-// };
-
-// export default GuideHome;
-
 import { Helmet } from "react-helmet-async";
 import SubscriptionCard from "./SubscriptionCard";
 
