4dad76652c63fe5f0079c858ecca3f6ad97a9b93
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-02 19:09:05 +0600

    [feat](guide): add guide get, add, update api

--- src/api/guide-api.js
+++ src/api/guide-api.js
@@ -2,18 +2,18 @@ import { axiosSecure } from "../hooks/axiosSecure";
 
 // Fetch Tour Guide details by Tour Guide email
 export const getTourGuide = async (email) => {
-  const { data } = await axiosSecure.get(`/tour-guide/${email}`);
+  const { data } = await axiosSecure.get(`/tour-guides/${email}`);
   return data;
 };
 
 // Add a new Tour Guide details
 export const addTourGuide = async (guideData) => {
-  const { data } = await axiosSecure.post("/tour-guide", guideData);
+  const { data } = await axiosSecure.post("/tour-guides", guideData);
   return data;
 };
 
 // Update Tour Guide profile details
 export const updateTourGuide = async (id, guideData) => {
-  const { data } = await axiosSecure.put(`/tour-guide/${id}`, guideData);
+  const { data } = await axiosSecure.put(`/tour-guides/${id}`, guideData);
   return data;
 };
