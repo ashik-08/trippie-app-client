6244189a0275914aafc0dd96cafa8d5244b53607
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-03 18:33:32 +0600

    [feat](guide-api): add getAllTourGuides function to guide-api

--- src/api/guide-api.js
+++ src/api/guide-api.js
@@ -1,4 +1,11 @@
 import { axiosSecure } from "../hooks/axiosSecure";
+import axiosInstance from "./axiosInstance";
+
+// get all tour guides
+export const getAllTourGuides = async ({ search }) => {
+  const { data } = await axiosInstance.get(`/tour-guides?search=${search}`);
+  return data;
+};
 
 // Fetch Tour Guide details by Tour Guide email
 export const getTourGuide = async (email) => {
