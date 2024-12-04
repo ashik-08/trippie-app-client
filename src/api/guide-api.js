86650ae63df59c0a12033402117f967a6ee5fd93
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-04 14:11:16 +0600

    [feat](guide): add api route to get guide details and sevices by guideId

--- src/api/guide-api.js
+++ src/api/guide-api.js
@@ -7,9 +7,15 @@ export const getAllTourGuides = async ({ search }) => {
   return data;
 };
 
+// get guide by id and services by guide id
+export const getGuideAndServices = async (id) => {
+  const { data } = await axiosInstance.get(`/tour-guides/${id}`);
+  return data;
+};
+
 // Fetch Tour Guide details by Tour Guide email
 export const getTourGuide = async (email) => {
-  const { data } = await axiosSecure.get(`/tour-guides/${email}`);
+  const { data } = await axiosSecure.get(`/tour-guides/email/${email}`);
   return data;
 };
 
