9af7e84f284817505be9cdb01ab4318c3d30f679
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-05 12:24:32 +0600

    [refactor](GuideCard): add 3 cards grid for lg device

--- src/components/TourGuide/TourGuideBoard.jsx
+++ src/components/TourGuide/TourGuideBoard.jsx
@@ -71,7 +71,7 @@ const TourGuideBoard = () => {
         <div className="flex justify-end items-center mt-8">
           <Search value={searchText} onSearch={handleSearch} />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 mt-8 md:mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8 md:mt-10">
           {renderGuide()}
         </div>
       </div>
