e21d8592db26af3dc8d8caebe257901306d0051e
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-09 23:30:22 +0600

    [feat](api): add update and delete appointment functions

--- src/api/appointment-api.js
+++ src/api/appointment-api.js
@@ -1,6 +1,21 @@
+import { axiosSecure } from "../hooks/axiosSecure";
 import axiosInstance from "./axiosInstance";
 
 export const createAppointment = async (appointmentData) => {
   const { data } = await axiosInstance.post("/appointments", appointmentData);
+  return data;
+};
+
+// update appointment status
+export const updateAppointmentStatus = async (appointmentId, status) => {
+  const { data } = await axiosSecure.put(`/appointments/${appointmentId}`, {
+    status,
+  });
+  return data;
+};
+
+// delete appointment by id
+export const deleteAppointment = async (appointmentId) => {
+  const { data } = await axiosSecure.delete(`/appointments/${appointmentId}`);
   return data;
 };
