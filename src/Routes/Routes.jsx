0c45e8d73e377e860bbc26e66b527b3adddf67f9
ashik-08 <ashikurrahman.8october@gmail.com>
2024-12-23 22:40:40 +0600

    [feat](Routes): uncomment About Us page route and its import

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../layout/DashboardLayout";
 import MainLayout from "../layout/MainLayout";
-// import AboutUsPage from "../pages/AboutUsPage/AboutUsPage";
+import AboutUsPage from "../pages/AboutUsPage/AboutUsPage";
 import ManageUsers from "../pages/Dashboard/Admin/ManageUsers";
 import AgencyProfile from "../pages/Dashboard/Agency/AgencyProfile";
 import CreateTour from "../pages/Dashboard/Agency/CreateTour";
@@ -59,10 +59,10 @@ export const routes = createBrowserRouter([
         path: "tour-guide/:guideId",
         element: <GuideDetailsPage />,
       },
-      // {
-      //   path: "about-us",
-      //   element: <AboutUsPage />,
-      // },
+      {
+        path: "about-us",
+        element: <AboutUsPage />,
+      },
     ],
   },
   {
